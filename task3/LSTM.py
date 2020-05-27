2532c5a5a88ca3abe7b6f192faf38d060c278537
Gujojo <gujh17@mails.tsinghua.edu.cn>
2020-05-27 11:19:55 +0800

    task3 complete

--- task3/LSTM.py
+++ task3/LSTM.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import json
 import torch
 import torch.nn as nn
@@ -6,14 +7,15 @@ import torch.nn.functional as F
 import torch.utils.data as Data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+from matplotlib import pyplot as plt
 
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = 'cpu'
 num_classes = 11
-batch_size = 2
-learning_rate = 2e-4
-num_epochs = 1000
+batch_size = 11
+learning_rate = 5e-5
+num_epochs = 600
 training_dir = ".\\Train"
 testing_dir = ".\\Test"
 label_dir = ".\\Test\\test\\test.json"
@@ -22,7 +24,7 @@ train_dataset = dset.ImageFolder(root=training_dir,
                                  transform=transforms.ToTensor())
 train_loader = Data.DataLoader(dataset=train_dataset,
                                batch_size=batch_size,
-                               shuffle=True,
+                               shuffle=False,
                                num_workers=0,
                                drop_last=True)
 test_dataset = dset.ImageFolder(root=testing_dir,
@@ -37,53 +39,76 @@ class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.DCNN = nn.Sequential(
-            # input: 64*64*3, output: 30*30*4
-            nn.Conv2d(in_channels=3, out_channels=4, kernel_size=4, stride=2, padding=0),
+            # input: 64*64*3, output: 30*30*8
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=4, stride=2, padding=0),
+            nn.BatchNorm2d(8),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False),
 
-            # input: 30*30*4, output: 13*13*8
-            nn.Conv2d(in_channels=4, out_channels=8, kernel_size=4, stride=2, padding=0),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False),
-
-            # input: 13*13*8, output: 4*4*16
+            # input: 30*30*8, output: 13*13*16
             nn.Conv2d(in_channels=8, out_channels=16, kernel_size=4, stride=2, padding=0),
+            nn.BatchNorm2d(16),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False),
 
-            # input: 4*4*16, output: 2*2*32
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, stride=1, padding=0),
+            # input: 13*13*16, output: 4*4*32
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=0),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False),
+
+            # input: 4*4*32, output: 2*2*32
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2, stride=1, padding=0),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False),
         )
 
-        # input: 2*2*32, output: 11
+        # input: 2*2*32, output: batch*1*30
         self.FC1 = nn.Sequential(
-            nn.Linear(in_features=32 * 2 * 2, out_features=num_classes, bias=True),
-            nn.ReLU(inplace=True)
+            nn.Linear(in_features=32 * 2 * 2, out_features=30, bias=True),
+            nn.BatchNorm1d(30),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.25)
         )
 
-        # input: 11, output: (bidirectional+1)*hidden_size
-        self.input_size = num_classes
+        # input: batch*1*30, output: batch*1*(bidirectional+1)*hidden_size
+        self.input_size = 30
         self.hidden_size = 20
-        self.num_layers = 1
+        self.num_layers = 2
         self.lstm = nn.LSTM(input_size=self.input_size,
                             hidden_size=self.hidden_size,
                             num_layers=self.num_layers,
                             batch_first=True,
-                            dropout=0,
                             bidirectional=True)
         self.hidden = self.init_hidden()
 
-        # input: (bidirectional+1)*hidden_size, output: 11
+        # input: batch*1*(bidirectional+1)*hidden_size, output: batch*1*11
         self.FC2 = nn.Sequential(
-            nn.Linear(in_features=2*self.hidden_size if self.lstm.bidirectional else self.hidden_size,
+            nn.Linear(in_features=2 * self.hidden_size if self.lstm.bidirectional else self.hidden_size,
                       out_features=num_classes,
                       bias=True),
+            nn.BatchNorm1d(1),
             nn.ReLU(inplace=True),
+            nn.Dropout(0.25),
             nn.Softmax(dim=2)
         )
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
 
     def init_hidden(self):
         if self.lstm.bidirectional:
@@ -105,11 +130,16 @@ class Model(nn.Module):
 
 
 model = Model().to(device)
-criterion = nn.CrossEntropyLoss()
+model.train()
+# criterion = nn.CrossEntropyLoss()
+# criterion = nn.NLLLoss()
+criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(),
                              lr=learning_rate,
-                             betas=(0.5, 0.999))
+                             betas=(0.6, 0.999),
+                             weight_decay=0.01)
 
+Loss = []
 for epoch in range(num_epochs):
     running_loss = 0
     cnt = 0
@@ -120,7 +150,10 @@ for epoch in range(num_epochs):
         model.hidden = model.init_hidden()
         # forward
         outputs = model(images)
-        loss = criterion(outputs, labels)
+        target = torch.zeros(batch_size, num_classes)
+        for n in range(batch_size):
+            target[n][labels[n]] = 1
+        loss = criterion(outputs, target)
         # backward
         tmp = loss.cpu()
         running_loss += tmp.data.numpy()
@@ -128,19 +161,50 @@ for epoch in range(num_epochs):
         optimizer.step()
         cnt += 1
     running_loss = running_loss / cnt
+    Loss.append(running_loss)
     if (epoch + 1) % 10 == 0:
         print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, running_loss))
 
+acc = 0
+cnt = 0
+reference = []
+for i, (images, labels) in enumerate(train_loader):
+    images = images.to(device)
+    labels = labels.to(device)
+    model.zero_grad()
+    model.hidden = model.init_hidden()
+    outputs = model(images)
+    reference = outputs.detach().numpy()
+    tmp = np.argmax(outputs.detach().numpy(), axis=1).tolist()
+    for n in range(len(tmp)):
+        cnt += 1
+        if tmp[n] == labels[n]:
+            acc += 1
+print(acc / cnt)
+
+# test
+model.eval()
 class_dict = train_dataset.class_to_idx
 pred = []
+pred_dict = {}
 for i, (images, labels) in enumerate(test_loader):
     images = images.to(device)
     model.zero_grad()
     model.hidden = model.init_hidden()
     outputs = model(images)
     file_name = test_loader.dataset.samples[i*batch_size: (i+1)*batch_size]
-    tmp = np.argmax(outputs.detach().numpy(), axis=1)
-    pred = pred + tmp.tolist()
+    tmp = []
+    for pic in range(num_classes):
+        mse = []
+        a = outputs[pic].detach().numpy()
+        for c in range(num_classes):
+            b = reference[c]
+            test = np.linalg.norm(a-b)
+            mse.append(test)
+        tmp.append(np.argmax(mse).tolist())
+    pred = pred + tmp
+    for n in range(batch_size):
+        pred_dict[file_name[n]] = tmp[n]
 
 with open(label_dir, 'r') as inFile:
     labelDict = json.load(inFile)
@@ -156,3 +220,10 @@ for i in range(len(test)):
         acc += 1
 acc /= len(test)
 print(acc)
+
+x = np.arange(0, num_epochs)
+plt.title("loss for each epoch")
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.plot(x, Loss)
+plt.show()
---
+++ task4/temp2coco.py
@@ -0,0 +1,124 @@
+import os
+import numpy as np
+import torch
+import json
+import math
+
+
+def temp2coco():
+    basicPath = os.sys.path[0]
+    trainPath = os.path.join(basicPath, "Detection\\train")
+    valPath = os.path.join(basicPath, "Detection\\val")
+    annoPath = os.path.join(basicPath, "Detection\\train_annotations.json")
+
+    classList = ['i2', 'i4', 'i5', 'io', 'ip', 'p11', 'p23', 'p26', 'p5', 'pl30',
+                 'pl40', 'pl5', 'pl50', 'pl60', 'pl80', 'pn', 'pne', 'po', 'w57']
+    class2id = {}  # 建立类和类编号的字典
+    for i in range(len(classList)):
+        class2id[classList[i]] = i
+
+    train_dirs = os.listdir(trainPath)
+    val_dirs = os.listdir(valPath)
+
+    with open(annoPath, 'r') as f:
+        anno = json.load(f)
+
+    train_outfile = os.path.join(basicPath, "train.json ")
+    val_outfile = os.path.join(basicPath, "val.json ")
+
+    train_coco = {}
+    train_coco['info'] = "spytensor created"
+    train_coco['license'] = ["license"]
+    train_coco['images'] = []
+    train_coco['annotations'] = []
+    train_coco['categories'] = []
+
+    for i in range(len(classList)):
+        cat = {}
+        cat['id'] = i
+        cat['name'] = classList[i]
+        train_coco['categories'].append(cat)
+
+    val_coco = {}
+    val_coco['info'] = "spytensor created"
+    val_coco['license'] = ["license"]
+    val_coco['images'] = []
+    val_coco['annotations'] = []
+    val_coco['categories'] = train_coco['categories']
+
+
+    sum_train = 0
+    sum_val = 0
+    for key in anno['imgs']:
+        img = {}
+        img["height"] = 2048
+        img["width"] = 2048
+        img["id"] = anno['imgs'][key]['id']
+        img["file_name"] = anno['imgs'][key]['path']
+
+        temp = str(key) + '.jpg'
+        if temp in train_dirs:
+            train_coco['images'].append(img)
+            for j in range(len(anno['imgs'][key]['objects'])):
+                target = anno['imgs'][key]['objects'][j]
+                obj = {}
+                obj['id'] = sum_train
+                obj['image_id'] = key
+                obj['category_id'] = class2id[target['category']]
+                obj['segementation'] = []
+
+                if 'ellipse_org' in target:
+                    for v in target['ellipse_org']:
+                        obj['segementation'].append(v[0])
+                        obj['segementation'].append(v[1])
+
+                obj['bbox'] = [math.floor(target['bbox']['xmin'])]
+                obj['bbox'].append(math.floor(target['bbox']['ymin']))
+                w = math.floor(target['bbox']['xmax'] - target['bbox']['xmin'])
+                h = math.floor(target['bbox']['ymax'] - target['bbox']['ymin'])
+                obj['bbox'].append(w)
+                obj['bbox'].append(h)
+
+                obj['iscrowd'] = 0
+                obj['area'] = float((w + 1)*(h + 1))
+
+                train_coco['annotations'].append(obj)
+                sum_train = sum_train + 1
+        else:
+            val_coco['images'].append(img)
+            for j in range(len(anno['imgs'][key]['objects'])):
+                target = anno['imgs'][key]['objects'][j]
+                obj = {}
+                obj['id'] = sum_val
+                obj['image_id'] = key
+                obj['category_id'] = class2id[target['category']]
+                obj['segementation'] = []
+
+                if 'ellipse_org' in target:
+                    for v in target['ellipse_org']:
+                        obj['segementation'].append(v[0])
+                        obj['segementation'].append(v[1])
+
+                obj['bbox'] = [math.floor(target['bbox']['xmin'])]
+                obj['bbox'].append(math.floor(target['bbox']['ymin']))
+                w = math.floor(target['bbox']['xmax'] - target['bbox']['xmin'])
+                h = math.floor(target['bbox']['ymax'] - target['bbox']['ymin'])
+                obj['bbox'].append(w)
+                obj['bbox'].append(h)
+
+                obj['iscrowd'] = 0
+                obj['area'] = float((w + 1) * (h + 1))
+
+                val_coco['annotations'].append(obj)
+                sum_val = sum_val + 1
+
+    with open(train_outfile, 'w') as f:
+        json.dump(train_coco, f)
+    with open(val_outfile, 'w') as f:
+        json.dump(val_coco, f)
+
+
+if __name__ =='__main__':
+    temp2coco()
+
+
